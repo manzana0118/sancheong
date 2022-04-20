ce962eca43e3df9f8102eb8e29b4ace6eebb0d7e
manzana00 <dev.manzana00@gmail.com>
2022-04-20 18:30:54 +0900

    모달창 추가 및 반응형 작업 중

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
-window.onload = function() {
+window.onload = function () {
 
     // aos 관련
     AOS.init({
         once: true
     });
 
+    // 모달창
+    let modal_close = $('.modal-close');
+    let modal = $('.modal');
+
+    modal_close.click(function () {
+        modal.hide();
+    });
+
+    let modal_bt = $('.modal-bt');
+    modal_bt.click(function () {
+        modal.show();
+    });
+
     // 패밀리 사이트 기능
     let family_hide = $('.family-hide');
-    let family_wrap =  $('.family-wrap');
+    let family_wrap = $('.family-wrap');
     let family = $('.family');
 
-    family_hide.click(function(){
+    family_hide.click(function () {
         family_wrap.fadeOut(300);
         // 스크롤바 디폴트 처리
         $('html').css('overflow-x', 'hidden');
         $('html').css('overflow-y', 'auto');
     });
-    
-    family.click(function(){
+
+    family.click(function () {
         family_wrap.fadeIn(300);
         // 스크롤바 없애기
         $('html').css('overflow', 'hidden');
     });
-    
+
     // 비주얼 슬라이드
-    let sw_visual = new Swiper ('.sw-visual', {
+    let sw_visual = new Swiper('.sw-visual', {
         effect: 'fade',
         autoplay: {
             delay: 2000,
@@ -58,78 +71,77 @@ window.onload = function() {
     });
 
     let sw_visual_pause = $('.sw-visual-pause')
-    sw_visual_pause.click(function(){
+    sw_visual_pause.click(function () {
         let temp = $(this).hasClass('sw-visual-pause-active');
-        if(temp != true) {
+        if (temp != true) {
             $(this).addClass('sw-visual-pause-active');
             sw_visual.autoplay.stop();
-        }else{
+        } else {
             $(this).removeClass('sw-visual-pause-active');
             sw_visual.autoplay.start();
         }
     });
 
     // 팝업존 슬라이드
-    let sw_notice_data = [
-        {
-            'imgurl' : 'popup_slide_01.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+    let sw_notice_data = [{
+            'imgurl': 'popup_slide_01.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_02.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_02.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_03.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_03.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_04.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_04.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_05.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_05.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_06.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_06.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_07.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_07.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_08.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_08.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_09.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_09.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         },
         {
-            'imgurl' : 'popup_slide_10.png',
-            'alt' : '팝업존',
-            'link' : '#',
-            'target' : '_self'
+            'imgurl': 'popup_slide_10.png',
+            'alt': '팝업존',
+            'link': '#',
+            'target': '_self'
         }
     ];
 
@@ -138,7 +150,7 @@ window.onload = function() {
 
     // html 만들기
     let sw_notice_html = '';
-    for(let i = 0; i < sw_notice_total; i++) {
+    for (let i = 0; i < sw_notice_total; i++) {
         let temp_data = sw_notice_data[i];
         sw_notice_html += '<div class=swiper-slide>';
         sw_notice_html += '<a href=';
@@ -149,7 +161,7 @@ window.onload = function() {
         sw_notice_html += ' alt='
         sw_notice_html += temp_data.alt
         sw_notice_html += ' class=sw-notice-link'
-        sw_notice_html +='>' 
+        sw_notice_html += '>'
         sw_notice_html += "</a>"
         sw_notice_html += '</div>';
     };
@@ -157,7 +169,6 @@ window.onload = function() {
     // 실제로 내용물 넣기
     let sw_notice_wrapper = $('.sw-notice .swiper-wrapper');
     sw_notice_wrapper.html(sw_notice_html);
-    console.log(sw_notice_html);
 
     let sw_notice = new Swiper('.sw-notice', {
         loop: true,
@@ -165,7 +176,7 @@ window.onload = function() {
             delay: 2000,
             disableOninteraction: false,
         },
-        slidesPerView: 2,
+        slidesPerView: 4,
         spaceBetween: 0,
         navigation: {
             nextEl: '.sw-notice-next',
@@ -185,16 +196,22 @@ window.onload = function() {
                     ' / ' +
                     '<span class="' + totalClass + '"></span>';
             }
-        }
+        },
+        breakpoints: {
+            1000: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+            },
+        },
     });
 
     let sw_notice_pause = $('.sw-notice-pause');
-    sw_notice_pause.click(function(){
+    sw_notice_pause.click(function () {
         let temp = $(this).hasClass('sw-notice-pause-active');
-        if(temp != true) {
+        if (temp != true) {
             $(this).addClass('sw-notice-pause-active');
             sw_notice.autoplay.stop();
-        }else{
+        } else {
             $(this).removeClass('sw-notice-pause-active');
             sw_notice.autoplay.start();
         }
@@ -214,7 +231,7 @@ window.onload = function() {
     // 투어 슬라이드 1 
     let sw_tour_1 = new Swiper('.sw-tour-01', {
         slidesPerView: 3,
-        allowTouchMove : false,
+        allowTouchMove: false,
         navigation: {
             prevEl: '.sw-tour-01 .sw-tour-prev',
             nextEl: '.sw-tour-01 .sw-tour-next',
@@ -226,7 +243,7 @@ window.onload = function() {
     // 투어 슬라이드 2
     let sw_tour_2 = new Swiper('.sw-tour-02', {
         slidesPerView: 3,
-        allowTouchMove : false,
+        allowTouchMove: false,
         navigation: {
             prevEl: '.sw-tour-02 .sw-tour-prev',
             nextEl: '.sw-tour-02 .sw-tour-next',
@@ -238,7 +255,7 @@ window.onload = function() {
     // 투어 슬라이드 3 
     let sw_tour_3 = new Swiper('.sw-tour-03', {
         slidesPerView: 3,
-        allowTouchMove : false,
+        allowTouchMove: false,
         navigation: {
             prevEl: '.sw-tour-03 .sw-tour-prev',
             nextEl: '.sw-tour-03 .sw-tour-next',
@@ -253,16 +270,16 @@ window.onload = function() {
     // 현재 선택된 슬라이드 순서번호
     let tour_show_index;
 
-    $.each(tour_menu, function(index, item) {
-        $(this).click(function(event){  
+    $.each(tour_menu, function (index, item) {
+        $(this).click(function (event) {
             event.preventDefault();
             tourSelect(index);
         });
     });
 
-    function tourSelect(_index){
+    function tourSelect(_index) {
         // 동일한 슬라이드를 보려고 하는 경우는 실행 하지 않는다.
-        if(_index == tour_show_index) {
+        if (_index == tour_show_index) {
             return;
         }
         // 보이는 슬라이드 번호 업데이트
@@ -306,17 +323,17 @@ window.onload = function() {
     let submenu_list_box = $('.submenu-list-box');
     let submenu_part_box = $('.submenu-part-box');
     let submenu_wrap = $('.submenu-wrap');
-    let submenu_wrap_height = [565, 510, 720, 410, 590, 235];
+    let submenu_wrap_height = [565, 510, 720, 410, 650, 235];
     let header_bottom = $('.header-bottom');
     let header = $('.header');
     // 선택된 메뉴를 저장한다.
     let header_index = 10000;
 
-    $.each(gnb_a, function(index, item){
+    $.each(gnb_a, function (index, item) {
 
-        $(this).mouseenter(function(){
+        $(this).mouseenter(function () {
 
-            if(index == header_index) {
+            if (index == header_index) {
                 return;
             }
             header_index = index;
@@ -339,7 +356,7 @@ window.onload = function() {
             submenu_list_box.eq(index).stop().fadeIn(200);
 
             submenu_part_box.hide();
-            if(index == 5) {
+            if (index == 5) {
                 submenu_part_box.stop().fadeIn(200);
             }
 
@@ -347,7 +364,7 @@ window.onload = function() {
 
         });
 
-        header_bottom.mouseleave(function(){
+        header_bottom.mouseleave(function () {
             submenu_wrap.css('height', 0);
             header.removeClass('header-focus');
             header_index = 10000;
@@ -364,15 +381,15 @@ window.onload = function() {
     // 2. 탭의 내용들을 저장해야 한다.
     let notice_tab_contents = $('.notice-box');
     // 3. 탭을 클릭해서 해당 내용을 보여주는 코드가 보여져야 한다.
-    $.each(notice_tab_menu, function(index, item) {
+    $.each(notice_tab_menu, function (index, item) {
 
-        $(this).click(function(event) {
+        $(this).click(function (event) {
 
             // a 태그의 href 막기
             event.preventDefault();
 
             // 내용물이 없는 경우 예외처리
-            if(index == 4) {
+            if (index == 4) {
                 return; // if 함수를 빠져나가게 하는 return
             }
 
@@ -394,9 +411,9 @@ window.onload = function() {
     // 2-1. 설명글 내용을 저장한다.
     let quick_tab_txt = $('.quick-desc span')
     // 3. 퀵메뉴 항목의 탭 메뉴를 클릭하면 내용을 보여준다.
-    $.each(quick_tab_menu, function(index, item) {
+    $.each(quick_tab_menu, function (index, item) {
 
-        $(this).click(function(event) {
+        $(this).click(function (event) {
 
             // a 태그의 href 막기
             event.preventDefault();
@@ -404,7 +421,7 @@ window.onload = function() {
             // 탭 메뉴
             quick_tab_menu.removeClass('quick-menu-focus');
             quick_tab_menu.eq(index).addClass('quick-menu-focus');
-            
+
             // 탭 메뉴 아이콘
             quick_tab_menu.removeClass('quick-menu-focus-icon-1');
             quick_tab_menu.eq(index).addClass('quick-menu-focus-icon-1');
@@ -428,28 +445,28 @@ window.onload = function() {
     let site_link_wrap = $('.site-link-wrap');
 
     // 3. site_bt 를 클릭하면 내용을 보여준다.
-    $.each(site_bt, function(index, item){
+    $.each(site_bt, function (index, item) {
 
         // 마우스 아웃 하면 사라지는 상태로 만든다.
         // css에서 padding 으로 조절해주어야 한다.
-        $(this).parent().mouseleave(function(){
+        $(this).parent().mouseleave(function () {
             // site_link_wrap.removeClass('site-link-wrap-focus');
         });
 
-        $(this).click(function(event) {
+        $(this).click(function (event) {
 
             // 이벤트를 밑으로 전달하지 않는다.
-            event.stopPropagation()    
+            event.stopPropagation()
 
             // 현재 클릭된것에 site-link-wrap-focus 적용되었나?
             let temp = site_link_wrap.eq(index).hasClass('site-link-wrap-focus');
-            if(temp) {
+            if (temp) {
                 //만약에 현재 클릭된 index와 같은 경우는
                 site_link_wrap.eq(index).removeClass('site-link-wrap-focus');
                 // 클릭 시 아이콘 변경
                 site_bt.eq(index).removeClass('f-site-bt-focus');
 
-            } else{
+            } else {
                 //그렇지 않은 경우는
                 site_link_wrap.removeClass('site-link-wrap-focus');
                 site_bt.removeClass('f-site-bt-focus');
@@ -457,14 +474,14 @@ window.onload = function() {
                 site_link_wrap.eq(index).addClass('site-link-wrap-focus');
                 site_bt.eq(index).addClass('f-site-bt-focus');
             }
-            
+
         });
     });
 
     // 클릭시 목록 숨기기
-    $('html, body').click(function(){
+    $('html, body').click(function () {
 
         site_link_wrap.removeClass('site-link-wrap-focus');
 
     });
-};    
+};
